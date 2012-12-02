15f45b7d9924fe26a52e47f1fb7d9ac3a86e431f
modiaditya <aditya.ucla@gmail.com>
2012-12-02 15:40:10 -0800

    adding test database program

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,21 @@
+import java.sql.SQLException;
+
 import org.json.JSONException;
 
+import me.navigation.server.DatabaseOperations;
 import me.navigation.shared.LatLong;
 
 
 public class Test {
 
-	public static void main(String args[]) throws JSONException
+	public static void main(String args[]) throws JSONException, SQLException
 	{
-		LatLong l = new LatLong(34.0,-118.2);
-		System.out.println(l.getJson().toString());
+		//   
+		LatLong l1 = new LatLong(34.059379,-118.448048);
+		LatLong l2 = new LatLong(34.059966,-118.446933);
+		
+		DatabaseOperations s = new DatabaseOperations();
+		s.getData(l1,l2);
+		
 	}
 }
