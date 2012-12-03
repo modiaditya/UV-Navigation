30f3ce0397db799a5d94115a16e91eb49b7c1118
modiaditya <aditya.ucla@gmail.com>
2012-12-02 18:01:49 -0800

    adding jar files

--- src/Test.java
+++ src/Test.java
@@ -8,14 +8,20 @@ import me.navigation.shared.LatLong;
 
 public class Test {
 
-	public static void main(String args[]) throws JSONException, SQLException
+	public static void main(String args[]) 
 	{
 		//   
 		LatLong l1 = new LatLong(34.059379,-118.448048);
 		LatLong l2 = new LatLong(34.059966,-118.446933);
 		
 		DatabaseOperations s = new DatabaseOperations();
+		try{
 		s.getData(l1,l2);
-		
+		}
+		catch(Exception e)
+		{
+			System.out.println("There is a database error");
+			System.out.println(e.getMessage());
+		}
 	}
 }
